a503de11ce398e60b9ae7d5da64675aeb484d8e3
smeet96 <smeetsherki@gmail.com>
2025-11-12 16:47:05 +0530

    folder_structure

--- src/components/Explorer.tsx
+++ src/components/Explorer.tsx
@@ -1,21 +1,44 @@
 import { useState } from 'react'
 import json from '../data.json'
 
-export const Explorer = () => {
-    const [data , setData] = useState(json)
-return (
-    <div>{
-data.map( node => {
-  return(
-    <div key={node.id}>
-        {node.name}
-    </div>
-  )  
-})
-}
-    </div>
 
+const Data = ({list}:any) => {
+    const [expanded , setExpanded] = useState(false)
+return (
+    <div>
+        {
+            list.map((node:any) => {
+                return (
+                    <div key={node.id}>
+                        {node.isFolder ? <span>+</span> : <span>-</span>}
+                        {node.name}
+                        <div>
+                            {node.children?.map((child:any) => {
+                                return (
+                                    <div key={child.id} className='pl-10'>
+                                        {expanded ? child.isFolder  ? "+"  : "-" : ""}
+                                        {expanded ? child.name : ""}
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                )
+            })
+        }
+    </div>
 )
 }
+
+export const Explorer = () => {
+    const [data , setData] = useState(json)
+    return (
+        <div>
+            <Data list={data} />
+        </div>
+    )
+}
+
+
 
 export default Explorer 
